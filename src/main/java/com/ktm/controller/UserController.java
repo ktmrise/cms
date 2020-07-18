bbe76ea8738b7aef25ce9f44b75c5e83a05793d6
ktmrise <2413114403@qq.com>
2020-07-18 21:11:29 +0800

    user

--- src/main/java/com/ktm/controller/UserController.java
+++ src/main/java/com/ktm/controller/UserController.java
@@ -5,13 +5,20 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.ktm.common.Result;
 import com.ktm.model.User;
 import com.ktm.service.IUserService;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 /**
  * <p>
@@ -29,6 +36,8 @@ public class UserController {
     @Resource
     private IUserService userService;
 
+    @Value("photo.path")
+    private String path;
 
     @RequestMapping("/login")
     public Result login(User user) {
@@ -49,7 +58,24 @@ public class UserController {
 
     @GetMapping("/findAllUser")
     public Result findAll() {
-       List<User> users= userService.findAllUser();
+        List<User> users = userService.findAllUser();
         return Result.ok(users);
+    }
+
+
+    @RequestMapping("/saveOrUpdateUser")
+    public Result saveOrUpdateUser(User user) throws IOException {
+
+
+//        String oldName = photo.getOriginalFilename();
+//        String newName = UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(oldName);
+//
+//        String realPath = path + newName;
+//        photo.transferTo(new File(realPath));
+//        user.setUserFace(realPath);
+        userService.saveOrUpdate(user);
+
+
+        return Result.ok("success", null, 200);
     }
 }
