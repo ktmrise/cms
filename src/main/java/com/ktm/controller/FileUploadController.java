61b2f1a5fae74f8e6b4603e84be3a05937f5fe69
ktmrise <2413114403@qq.com>
2020-07-19 16:47:43 +0800

    fileupload half

---
+++ src/main/java/com/ktm/controller/FileUploadController.java
@@ -0,0 +1,36 @@
+package com.ktm.controller;
+
+
+import com.ktm.common.Result;
+import org.apache.commons.io.FilenameUtils;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@Controller
+@RequestMapping("/manager")
+public class FileUploadController {
+
+    @Value("${photo.path}")
+    private String path;
+
+
+    @PostMapping("/file/upload")
+    public Result fileUpload(MultipartFile file, HttpSession session) throws IOException {
+        System.out.println(file.getOriginalFilename());
+
+        String oldName = file.getOriginalFilename();
+        String newName = UUID.randomUUID().toString() + "." + FilenameUtils.getExtension(oldName);
+        String realPath = path + newName;
+        file.transferTo(new File(realPath));
+        session.setAttribute("faceUrl","http://localhost:8099/"+newName);
+        return Result.ok("http://localhost:8099/"+newName);
+    }
+}
--- src/main/java/com/ktm/controller/UserController.java
+++ src/main/java/com/ktm/controller/UserController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
@@ -36,8 +37,7 @@ public class UserController {
     @Resource
     private IUserService userService;
 
-    @Value("photo.path")
-    private String path;
+
 
     @RequestMapping("/login")
     public Result login(User user) {
@@ -64,7 +64,7 @@ public class UserController {
 
 
     @RequestMapping("/saveOrUpdateUser")
-    public Result saveOrUpdateUser(User user) throws IOException {
+    public Result saveOrUpdateUser(User user, HttpSession session) throws IOException {
 
 
 //        String oldName = photo.getOriginalFilename();
@@ -73,6 +73,8 @@ public class UserController {
 //        String realPath = path + newName;
 //        photo.transferTo(new File(realPath));
 //        user.setUserFace(realPath);
+        String faceUrl = (String) session.getAttribute("faceUrl");
+        user.setUserFace(faceUrl);
         userService.saveOrUpdate(user);
 
 
